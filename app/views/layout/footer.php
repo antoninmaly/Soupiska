1852faa6cb590a9c5c52683ce28d9cd1da71ccf8
Antonín Malý <antonin.maly@tul.cz>
2026-05-18 20:08:19 +0200

    Footer - zmena designu

--- app/views/layout/footer.php
+++ app/views/layout/footer.php
@@ -1,10 +1,10 @@
-<footer class="mt-auto border-t border-gray-200 py-10 bg-white">
-        <div class="container mx-auto px-6 text-center">
-            <p class="text-slate-400 text-xs tracking-[0.3em] uppercase font-bold italic">
-                &copy; WA 2026 Antonín Malý - <span class="text-blue-600">Soupiska</span>
-            </p>
-        </div>
-    </footer>
+<footer class="mt-auto bg-gradient-to-r from-blue-900 to-blue-700 py-8 border-t border-blue-800 shadow-inner">
+    <div class="container mx-auto px-6 text-center">
+        <p class="text-blue-200 text-xs tracking-[0.3em] uppercase font-bold italic">
+            &copy; WA 2026 Antonín Malý - <span class="text-white font-black">Soupiska</span>
+        </p>
+    </div>
+</footer>
 
 </body>
 </html>
